db1b779af2c74fb09caa27c33f94a1cffaf2d888
tuningmind <tuningmind@gmail.com>
2017-03-25 19:33:23 -0600

    Create buttons inside done() call

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -2,36 +2,29 @@ window.onload = function stuff() {
 
 const url = `http://007api.co/api/gadgets/`
 
-const addButton = function (sometext) {
-  const gadgetname = $("<button></button>").text(sometext)
-  $("#buttons").append(gadgetname)
+const addButton = function (sometext, i) {
 }
-let addDescription = function (sometext) {
+let handleClick = function (sometext) {
   const description = $("<div></div>").text(sometext)
   $("#description").append(description)
 }
-// let handleClick = function () {
-//   button.textContent = gadgets[button.name].description
-// }
-// $('#buttons').on('click', 'button', handleClick);
 
   $.ajax({
     url: url
   }).done(function(data) {
       const results = data.results
-      console.log("results: ", results)
       const resultsLength = results.length;
       const gadgets = [];
       const descriptions = [];
 
       for (let i = 0; i < resultsLength; i++) {
         gadgets.push(results[i].name)
-        const p = document.createElement('p')
-        addButton(results[i].name)
-        descriptions.push(results[i].description)
-    console.log("descriptions: ", descriptions)
+        gadgetname = $("<button></button>").text(results[i].name)
+        $("#buttons").append(gadgetname)
       }
-      
+      $('button').click( function () {
+        console.log("inside handleClick")
+      })
     })
 
 }
