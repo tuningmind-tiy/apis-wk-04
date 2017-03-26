bdbeff23be7940679440e48b6ac9dedb0909cca1
tuningmind <tuningmind@gmail.com>
2017-03-25 22:11:26 -0600

    Replace button image with button styling

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,13 +1,8 @@
 window.onload = function stuff() {
 
 const url = `http://007api.co/api/gadgets/`
+const alice = "Alice was beginning to get very tired of sitting by her sister on the bank and of having nothing to do; once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is theuse of a book,' thought Alice, 'without pictures or conversation?' So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her."
 
-const addButton = function (sometext, i) {
-}
-let handleClick = function (sometext) {
-  const description = $("<div></div>").text(sometext)
-  $("#description").append(description)
-}
 
   $.ajax({
     url: url
@@ -21,7 +16,8 @@ let handleClick = function (sometext) {
         $("#buttons").append(gadgetname)
       }
       $('button').click( function () {
-        console.log("inside handleClick")
+        $('#descriptions').text(alice)
+
       })
     })
 
